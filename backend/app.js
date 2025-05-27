90389109b8062b5ee478f98a35a989040cdb5f60
prince-p2111 <princegangadiya99k@gmail.com>
2025-05-27 11:40:23 +0530

    Initialize backend project with package.json and essential dependencies

---
+++ backend/app.js
@@ -0,0 +1,45 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
+const db = require("./models/index");
+const cors = require("cors");
+const path = require("path");
+const morgan = require("morgan");
+const cookieParser = require("cookie-parser");
+
+// cors settings
+app.use(
+  cors({
+    origin: ["http://localhost:5173"],
+    credentials: true,
+    allowedHeaders: [
+      "Content-Type",
+      "Authorization",
+      "Cache-Control",
+      "Expires",
+      "Pragma",
+    ],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+  })
+);
+
+// Middleware to parse JSON request bodies
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, "public")));
+app.use(morgan("dev"));
+
+// Routes
+
+app.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
+});
+
+// Error handling middleware
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).send("Something broke!");
+});
+
+module.exports = app;
